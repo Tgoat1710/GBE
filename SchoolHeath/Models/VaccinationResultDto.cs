bbe349579259987890d38eae02c5ab1d5b6ec0d5
Tgoat1710 <trongmaster1710@gmail.com>
2025-07-12 13:01:42 +0700

    thêm model để xem kết quả

---
+++ SchoolHeath/Models/VaccinationResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolHeath.Models.Dtos
+{
+    public class VaccinationResultDto
+    {
+        public int VaccinationId { get; set; }
+        public int CampaignId { get; set; }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; } = null!;
+        public string VaccineName { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public DateTime? DateOfVaccination { get; set; }
+        public int? AdministeredBy { get; set; }
+    }
+}
