6b8865c9174f925915a8e6b0cb1293f77bcaf582
Tgoat1710 <trongmaster1710@gmail.com>
2025-07-12 13:01:30 +0700

    tạo thêm

---
+++ SchoolHeath/Models/VaccinationRecordDto.cs
@@ -0,0 +1,9 @@
+﻿public class VaccinationRecordDto
+{
+    public int StudentId { get; set; }
+    public int CampaignId { get; set; }
+    public string VaccineName { get; set; }
+    public string Status { get; set; }
+    public DateTime DateOfVaccination { get; set; }
+    public int AdministeredBy { get; set; }
+}
