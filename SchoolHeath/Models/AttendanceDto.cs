86fbc98bceeb7a83c403b840b782fcdf8907867e
Tgoat1710 <trongmaster1710@gmail.com>
2025-07-12 12:59:52 +0700

    thêm model

---
+++ SchoolHeath/Models/AttendanceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolHeath.Models
+{
+    public class AttendanceDto
+    {
+        public int StudentId { get; set; }
+        public int NurseId { get; set; }
+        public bool Present { get; set; }
+    }
+}
