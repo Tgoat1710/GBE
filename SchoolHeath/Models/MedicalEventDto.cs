1c8d0d21cca078d5c8e15584a0c94b78d7104262
Tgoat1710 <trongmaster1710@gmail.com>
2025-07-12 13:00:13 +0700

    thêm

---
+++ SchoolHeath/Models/MedicalEventDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolHeath.Models
+{
+    public class MedicalEventDto
+    {
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        public string EventType { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? Outcome { get; set; }
+        public string? Notes { get; set; }
+        public string? UsedSupplies { get; set; }
+        public int? HandledBy { get; set; }
+        // Thêm các trường khác nếu cần
+    }
+}
