9809247b8851898fedf30f4c2b8af76e5da1f586
Tgoat1710 <trongmaster1710@gmail.com>
2025-07-14 21:20:42 +0700

    thêm function cho luồng mới

---
+++ SchoolHeath/Models/HealthCampaignDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SchoolHeath.Models
+{
+    public class HealthCampaignDto
+    {
+        public int CampaignId { get; set; }
+        public string? Name { get; set; }
+        public DateTime StartDate { get; set; }      // Ngày khám
+        public DateTime EndDate { get; set; }
+        public string? Description { get; set; }
+        public int? NurseId { get; set; }
+        public string? NurseName { get; set; }
+        public string? TargetClass { get; set; }      // Thêm trường lớp
+        public string? Status { get; set; }           // Thêm trường trạng thái
+    }
+}
